4d36a8ca52b2e11c7fdafd88a0263578752cc86c
fredokun <frederic.peschanski@lip6.fr>
2020-02-04 14:29:00 +0100

    project

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/yacw-central/yaw-reactive"
   :license {:name "The MIT License"
             :url  "http://opensource.org/licenses/MIT"}
-  :source-paths ["src"]
+  :source-paths ["src", "examples"]
   :test-paths ["test" "src/test"]
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [yaw-engine "0.3.0-SNAPSHOT"]]
