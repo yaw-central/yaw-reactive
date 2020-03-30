e4d403f39a464ae3f8340adbb5bfd41d8b43de04
Frederic Peschanski <fredokun@github.com>
2020-03-30 17:31:26 +0200

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yaw-reactive "0.4.0-SNAPSHOT"
+(defproject yaw-reactive "0.5.0-SNAPSHOT"
   :description "A FRP framework for (simple) 3D games in Clojure."
   :url "https://github.com/yacw-central/yaw-reactive"
   :license {:name "The MIT License"
